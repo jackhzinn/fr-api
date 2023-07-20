8a5a89357ff9000610f426b7cb0d5a81e84e2708
jackhzinn <math.zinn@gmail.com>
2023-07-20 15:26:08 -0400

    WIP... not quite working yet

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,46 @@
+
+
+const handleDetectAPI = (req, res) => {
+	/* ClarifAI setup */
+    const PAT = '45148ef82a8848ce9f73d68c9f0c5639';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'jackzinn';       
+    const APP_ID = 'facial-recognition';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": req.body.input
+                  }
+              }
+          }
+      ]
+    });
+    
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+   };
+console.log('about to fetch from Clarifai', requestOptions)
+	fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+		.then(data => {console.log('returned data', data); return res.json(data) })
+		.catch(err => {console.log('errored',err); res.status(400).json('Facial Detection API Error') })		
+};
+
+
+
 const handleImage = (req, res, db) => {
 	const {id} = req.body;
 	db('users')
@@ -11,5 +54,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage
+	handleImage,
+	handleDetectAPI
 }
--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id_email', (req, res) => { profile.handleProfileGet(req, res, db) });
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageDetect', (req, res) => { image.handleDetectAPI(req, res) });
+
 
 app.listen(3000, ()=>{
 	console.log('app is running');
